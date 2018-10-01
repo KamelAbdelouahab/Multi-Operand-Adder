34343e786ffb6c992eaa015aa0584c7bda57e34a
Kamel Abdelouahab <kamel_eddine.abdelouahab@uca.fr>
2018-10-01 14:51:24 +0200

    Studying the impact of Quartus fitter optimizations

--- src/DataTypes.vhd
+++ src/DataTypes.vhd
@@ -6,7 +6,7 @@ library ieee;
 package DataTypes is
     constant CONST_DATA_WIDTH     : natural := 4;    -- Bitwidth
     constant CONST_NUM_OPERANDS   : natural := 128;  -- Number of operands
-    constant CONST_PIPLINE_STAGES : natural := 5;    -- Pipeline depth
+    constant CONST_PIPLINE_STAGES : natural := 3;    -- Pipeline depth
     constant CONST_SUM_WIDTH      : natural := CONST_DATA_WIDTH + natural(ceil(log2(real(CONST_NUM_OPERANDS))));
     type data_array is array (natural range <>) of std_logic_vector (CONST_DATA_WIDTH-1 downto 0);
     type sum_array is array (natural range <>) of std_logic_vector (CONST_SUM_WIDTH-1 downto 0);
